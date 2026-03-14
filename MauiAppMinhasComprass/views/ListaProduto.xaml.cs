907a7a4b98a645c7b68938b698e5b8aa373e7c1f
Isabela Miyuki <isamiyuki1754@gmail.com>
2026-03-14 12:41:42 -0300

    Adição do botão de pesquisa e a criação da tabela.

--- MauiAppMinhasComprass/views/ListaProduto.xaml.cs
+++ MauiAppMinhasComprass/views/ListaProduto.xaml.cs
@@ -1,21 +1,95 @@
+using MauiAppMinhasComprass.models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasComprass.views;
 
 public partial class ListaProduto : ContentPage
 {
-	public ListaProduto()
-	{
-		InitializeComponent();
-	}
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
+    public ListaProduto()
+    {
+        InitializeComponent();
+
+        lst_produtos.ItemsSource = lista;
+    }
+
+    protected async override void OnAppearing()
+    {
+        try
+        {
+            lista.Clear();
+
+            List<Produto> tmp = await App.Db.GetAll();
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
-		try
-		{
-			Navigation.PushAsync(new views.NovoProduto());
+        try
+        {
+            Navigation.PushAsync(new views.NovoProduto());
 
-		} catch (Exception ex)
-		{
-			DisplayAlert("Ops", ex.Message, "OK");
-		}
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        try
+        {
+            string q = e.NewTextValue;
+
+            lista.Clear();
+
+            List<Produto> tmp = await App.Db.Search(q);
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(i => i.Total);
+
+        string msg = $"O total é {soma:C}";
+
+        DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    private async void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            MenuItem selecinado = sender as MenuItem;
+
+            Produto p = selecinado.BindingContext as Produto;
+
+            bool confirm = await DisplayAlert(
+                "Tem Certeza?", $"Remover {p.Descricao}?", "Sim", "Não");
+
+            if (confirm)
+            {
+                await App.Db.Delete(p.Id);
+                lista.Remove(p);
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
